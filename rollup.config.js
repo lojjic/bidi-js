1b4718e06b4a472ea820b0df9ede388f8b7a26ae
Jason Johnston <jason.johnston@protectwise.com>
2021-04-14 21:59:53 -0600

    dist files now contain a single self-contained factory function

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,26 @@
 import { terser } from 'rollup-plugin-terser'
 
+const banner = `export default function bidi() {`
+const footer = `return bidi}`
+
 export default {
-  input: 'src/bidi.js',
+  input: 'src/index.js',
   output: [
     {
       file: 'dist/bidi.js',
-      format: 'umd',
-      name: 'bidi'
+      format: 'iife',
+      name: 'bidi',
+      sourcemap: true,
+      banner,
+      footer
     },
     {
       file: 'dist/bidi.min.js',
-      format: 'umd',
+      format: 'iife',
       name: 'bidi',
+      sourcemap: true,
+      banner,
+      footer,
       plugins: [
         terser({
           ecma: 5,
