e7dfa5e9d6f226e358117c22ee7a0fa509cca818
Jason Johnston <jason.johnston@protectwise.com>
2021-04-15 16:47:25 -0600

    fix: produce esm and umd build files

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,52 @@
 import { terser } from 'rollup-plugin-terser'
 
-const banner = `export default function bidi() {`
-const footer = `return bidi}`
-
-export default {
-  input: 'src/index.js',
-  output: [
-    {
+export default [
+  // First compile to an iife, and wrap the whole thing into an exported factory function.
+  // This ensures all the code is self-contained within that one factory function.
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/bidi.js',
       format: 'iife',
       name: 'bidi',
-      sourcemap: true,
-      banner,
-      footer
-    },
-    {
-      file: 'dist/bidi.min.js',
-      format: 'iife',
-      name: 'bidi',
-      sourcemap: true,
-      banner,
-      footer,
-      plugins: [
-        terser({
-          ecma: 5,
-          mangle: {properties: {regex: /^_/}}
-        })
-      ]
+      banner: `export default function bidiFactory() {`,
+      footer: `return bidi}`
     }
-  ]
-}
+  },
+  // Then wrap that exported factory function as esm and umd
+  {
+    input: 'dist/bidi.js',
+    output: [
+      {
+        file: 'dist/bidi.mjs',
+        format: 'esm'
+      },
+      {
+        file: 'dist/bidi.min.mjs',
+        format: 'esm',
+        plugins: [
+          terser({
+            ecma: 5,
+            mangle: {properties: {regex: /^_/}}
+          })
+        ]
+      },
+      {
+        file: 'dist/bidi.js',
+        format: 'umd',
+        name: 'bidi_js'
+      },
+      {
+        file: 'dist/bidi.min.js',
+        format: 'umd',
+        name: 'bidi_js',
+        plugins: [
+          terser({
+            ecma: 5,
+            mangle: {properties: {regex: /^_/}}
+          })
+        ]
+      }
+    ]
+  }
+]
